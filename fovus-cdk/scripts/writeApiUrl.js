b01480774dc4dfdb419542da55f1e6f60e3c2f63
PreethiBompally <pb54108@uga.edu>
2024-07-26 07:59:44 -0400

    rectified v2 warning

--- fovus-cdk/scripts/writeApiUrl.js
+++ fovus-cdk/scripts/writeApiUrl.js
@@ -1,24 +1,23 @@
-const AWS = require('aws-sdk');
+const { CloudFormationClient, DescribeStacksCommand } = require("@aws-sdk/client-cloudformation");
 const fs = require('fs');
 const path = require('path');
 
-AWS.config.update({ region: 'us-east-2' });
-
-const cloudformation = new AWS.CloudFormation();
+const cloudFormationClient = new CloudFormationClient({ region: 'us-east-2' });
 const stackName = 'FovusCdkStack';
 
 async function getApiUrl() {
   try {
-    const data = await cloudformation.describeStacks({ StackName: stackName }).promise();
-    const outputs = data.Stacks[0].Outputs;
+    const command = new DescribeStacksCommand({ StackName: stackName });
+    const response = await cloudFormationClient.send(command);
+    const outputs = response.Stacks[0].Outputs;
     const apiUrlOutput = outputs.find(output => output.OutputKey === 'ApiUrl');
     const apiUrl = apiUrlOutput.OutputValue;
     const dynamoUrlOutput = outputs.find(output => output.OutputKey === 'DynamoDbApiUrl');
     const dynamoUrl = dynamoUrlOutput.OutputValue;
 
     const currentDir = __dirname;
-    const parentDir = path.resolve(path.resolve(currentDir, '..'),'..');
-    const outputDir = path.join(parentDir, 'fovus-react-app/public');
+    const parentDir = path.resolve(path.resolve(currentDir, '..'), '..');
+    const outputDir = path.join(parentDir, 'public');
     const filePath = path.join(outputDir, 'api_url.txt');
 
     if (!fs.existsSync(outputDir)) {
